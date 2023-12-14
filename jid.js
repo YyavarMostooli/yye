aa762a46a7d2bc595427e75a18127601317e0b12
YyavarMostooli <138663768+YyavarMostooli@users.noreply.github.com>
2023-12-14 23:34:12 +1300

    Create jid.js

---
+++ jid.js
@@ -0,0 +1,23 @@
+const container = document.getElementById('sandy');
+const root = ReactDOM.createRoot(container);
+root.render(<p>Halelo</p>);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Car />);
+
+            class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+
+const container = document.getElementById('sandy');
+const root = ReactDOM.createRoot(container);
+root.render(<p>Halleo</p>);
+
+const container = document.getElementById('sandy');
+const root = ReactDOM.createRoot(container);
+root.render(<p>Hallot34</p>);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Car />);
