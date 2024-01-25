e1fa8e0ac9aab17d03a121b422641bc3a1e2ce81
YyavarMostooli <138663768+YyavarMostooli@users.noreply.github.com>
2024-01-26 06:42:51 +1300

    Update jid.js

--- jid.js
+++ jid.js
@@ -2,7 +2,7 @@ const container = document.getElementById('sny');
 const roots = ReactDOM.createeRoot(container);
 root.render(<p>Halelo</p>);
 
-const root = ReactDOM.createRoot(document.getElemeById('rot'));
+const root = ReactDOM.createRoot(document.getElemeById('rooot'));
 root.render(<Car />);
 
             class Car extends React.Component {
