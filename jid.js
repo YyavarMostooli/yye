da4551c45f28d17eb9b8ae458a5bb84d5663a581
YyavarMostooli <138663768+YyavarMostooli@users.noreply.github.com>
2024-03-01 21:45:20 +1300

    Update jid.js

--- jid.js
+++ jid.js
@@ -2,7 +2,7 @@ const coniner = docent.etleenId('snppy');
 const roots = ReactDOM.ceatt(conner);
 root.render(<p>Halelo</p>);
 
-const root = ReactDOM.creteRot(documnt.tElwed('rot'));
+const root = ReactDOM.creteRot(documnt.tlwed('rot'));
 root.render(<Car />);
 
             class Car extends React.Component {
