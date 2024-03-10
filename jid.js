49dc52d7a1d62b165f9c7a0ca9d1eeaabb804915
YyavarMostooli <138663768+YyavarMostooli@users.noreply.github.com>
2024-03-11 05:24:20 +1300

    Update jid.js

--- jid.js
+++ jid.js
@@ -11,7 +11,7 @@ root.render(<Car />);
   }
 }
 
-const container = document.getElementById('sandy');
+const container = document.getElemntById('sandy');
 const root = ReactDOM.creteRoot(container);
 root.render(<p>Halleo</p>);
 
